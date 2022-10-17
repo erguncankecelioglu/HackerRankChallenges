40fc9ce470f7902943e6bb7939fff07de1b847c9
Ergüncan <erguncan06@gmail.com>
2022-10-17 15:23:16 +0300

    done

---
+++ GradingStudents.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class GradingStudents{
+        public static List<Integer> gradingStudents(List<Integer> grades) {
+                for (int i = 0; i < grades.size(); i++) {
+                        if(grades.get(i)>=38){
+                                if (grades.get(i)%5!=0){
+                                        if((grades.get(i)+1)%5==0){
+                                                grades.set(i, grades.get(i) + 1);
+                                        }
+                                        else if((grades.get(i)+2)%5==0){
+                                                grades.set(i, grades.get(i) + 2);
+                                        }
+                                }
+                        }
+                }
+                return grades;
+
+        }
+}
